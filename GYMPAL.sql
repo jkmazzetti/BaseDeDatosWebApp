dbd744b0275407f83897232dc4c7efdec642e605
Jeremaz <jere.mazzetti@gmail.com>
2022-09-04 03:31:54 -0300

    Actualizaciones

--- GYMPAL.sql
+++ GYMPAL.sql
@@ -1,5 +1,15 @@
 CREATE DATABASE `GYMPAL` CHARACTER SET utf8 COLLATE utf8_bin;
 USE `GYMPAL`;
+CREATE TABLE `EstadosObjetivos` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+ `Estado` varchar(12) COLLATE utf8_bin DEFAULT NULL,
+ PRIMARY KEY (`id`)
+);
+CREATE TABLE `EstadosSubrutina` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+ `Estado` varchar(12) COLLATE utf8_bin DEFAULT NULL,
+ PRIMARY KEY (`id`)
+);
 CREATE TABLE `DiasHabilitados` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
  `Dia` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -8,11 +18,6 @@ CREATE TABLE `DiasHabilitados` (
 CREATE TABLE `Sexo` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
  `Sexo` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
- PRIMARY KEY (`id`)
-);
-CREATE TABLE `Estados` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `Estado` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
  PRIMARY KEY (`id`)
 );
 CREATE TABLE `GruposMusculares` (
@@ -49,7 +54,7 @@ CREATE TABLE `Objetivos` (
  KEY `id_tipo` (`id_tipo`),
  KEY `id_estado` (`id_estado`),
  CONSTRAINT `Objetivos_ibfk_1` FOREIGN KEY (`id_tipo`) REFERENCES `TiposDeObjetivos` (`id`),
- CONSTRAINT `Objetivos_ibfk_3` FOREIGN KEY (`id_estado`) REFERENCES `Estados` (`id`)
+ CONSTRAINT `Objetivos_ibfk_2` FOREIGN KEY (`id_estado`) REFERENCES `EstadosObjetivos` (`id`)
 );
 CREATE TABLE `Preguntas` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -62,28 +67,43 @@ CREATE TABLE `Usuarios` (
  `Apellido` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
  `Sexo` int(2) NOT NULL,
  `FechaDeNacimiento` date DEFAULT NULL,
- `DNI` int(11) DEFAULT NULL,
+ `DNI` varchar(11) DEFAULT NULL,
  `Direccion` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `Email` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
  `Celular` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
- `UserName` varchar(16) COLLATE utf8_unicode_ci DEFAULT NULL,
- `Password` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `FechaDeRegistro` date NOT NULL,
+ PRIMARY KEY (`id`),
+ KEY `Sexo` (`Sexo`),
+ CONSTRAINT `Usuarios_ibfk_1` FOREIGN KEY (`Sexo`) REFERENCES `Sexo` (`id`)
+);
+CREATE TABLE `EstadosUsuarios` (
+ `id_usuario` int(11) NOT NULL,
+ `id_sexo` int(11) NOT NULL,
+ `Altura` float NOT NULL,
+ `Peso` float NOT NULL,
+ `IndicenMasaCorporal` float DEFAULT NULL,
+ `PorcentajeGrasaCorporal` float DEFAULT NULL,
+ KEY `id_usuario` (`id_usuario`),
+ KEY `id_sexo` (`id_sexo`),
+ CONSTRAINT `EstadosUsuarios_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios` (`id`),
+ CONSTRAINT `EstadosUsuarios_ibfk_2` FOREIGN KEY (`id_sexo`) REFERENCES `Sexo` (`id`)
+);
+CREATE TABLE `LogIn` (
+ `id_usuario` int(11) NOT NULL,
+ `UserName` varchar(32) COLLATE utf8_bin NOT NULL,
+ `Password` varchar(32) COLLATE utf8_bin NOT NULL,
  `PrimeraPregunta` int(11) NOT NULL,
  `SegundaPregunta` int(11) NOT NULL,
  `TerceraPregunta` int(11) NOT NULL,
- `Peso` float DEFAULT NULL,
- `PorcentajeGrasaCorporal` float DEFAULT NULL,
- `IndiceMasaCorporal` float DEFAULT NULL,
  `Provisoria` tinyint(1) DEFAULT 0,
- `FechaDeRegistro` date NOT NULL,
- PRIMARY KEY (`id`),
+ KEY `id_usuario` (`id_usuario`),
  KEY `PrimeraPregunta` (`PrimeraPregunta`),
  KEY `SegundaPregunta` (`SegundaPregunta`),
  KEY `TerceraPregunta` (`TerceraPregunta`),
- KEY `Sexo` (`Sexo`),
- CONSTRAINT `Usuarios_ibfk_1` FOREIGN KEY (`PrimeraPregunta`) REFERENCES `Preguntas` (`id`),
- CONSTRAINT `Usuarios_ibfk_2` FOREIGN KEY (`SegundaPregunta`) REFERENCES `Preguntas` (`id`),
- CONSTRAINT `Usuarios_ibfk_3` FOREIGN KEY (`TerceraPregunta`) REFERENCES `Preguntas` (`id`),
- CONSTRAINT `Usuarios_ibfk_4` FOREIGN KEY (`Sexo`) REFERENCES `Sexo` (`id`)
+ CONSTRAINT `LogIn_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios` (`id`),
+ CONSTRAINT `LogIn_ibfk_2` FOREIGN KEY (`PrimeraPregunta`) REFERENCES `Preguntas` (`id`),
+ CONSTRAINT `LogIn_ibfk_3` FOREIGN KEY (`SegundaPregunta`) REFERENCES `Preguntas` (`id`),
+ CONSTRAINT `LogIn_ibfk_4` FOREIGN KEY (`TerceraPregunta`) REFERENCES `Preguntas` (`id`)
 );
 CREATE TABLE `RespuestasDeUsuarios` (
  `id_usuario` int(11) DEFAULT NULL,
@@ -116,14 +136,11 @@ CREATE TABLE `Entrenamientos` (
  `FrecuenciaDePesaje` int(11) DEFAULT 0,
  `SemanasDeDuracion` int(11) DEFAULT 4,
  `id_objetivo` int(11) NOT NULL, 
- `id_estado` int(11) NOT NULL,
  PRIMARY KEY (`id`),
  KEY `id_usuario` (`id_usuario`),
  KEY `id_objetivo` (`id_objetivo`), 
- KEY `id_estado` (`id_estado`),
  CONSTRAINT `Entrenamientos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios` (`id`),
- CONSTRAINT `Entrenamientos_ibfk_2` FOREIGN KEY (`id_objetivo`) REFERENCES `Objetivos` (`id`), 
- CONSTRAINT `Entrenamientos_ibfk_3` FOREIGN KEY (`id_estado`) REFERENCES `Estados` (`id`)
+ CONSTRAINT `Entrenamientos_ibfk_2` FOREIGN KEY (`id_objetivo`) REFERENCES `Objetivos` (`id`) 
 );
 CREATE TABLE `LogrosDeUsuarios` (
  `id_usuario` int(11) NOT NULL,
@@ -143,14 +160,11 @@ CREATE TABLE `Rutinas` (
  `TiempoRestante` float DEFAULT 0.0,
  `Completitud` float DEFAULT 0.0,
  `id_entrenamiento` int(11) NOT NULL,
- `id_estado` int(11) DEFAULT 1,
  PRIMARY KEY (`id`),
  KEY `id_dia` (`id_dia`),
  KEY `id_entrenamiento` (`id_entrenamiento`),
- KEY `id_estado` (`id_estado`),
  CONSTRAINT `Rutinas_ibfk_1` FOREIGN KEY (`id_dia`) REFERENCES `DiasHabilitados` (`id`),
- CONSTRAINT `Rutinas_ibfk_2` FOREIGN KEY (`id_entrenamiento`) REFERENCES `Entrenamientos` (`id`),
- CONSTRAINT `Rutinas_ibfk_3` FOREIGN KEY (`id_estado`) REFERENCES `Estados` (`id`)
+ CONSTRAINT `Rutinas_ibfk_2` FOREIGN KEY (`id_entrenamiento`) REFERENCES `Entrenamientos` (`id`)
 );
 CREATE TABLE `Subrutinas` (
  `id_rutina` int(11) NOT NULL,
@@ -166,7 +180,7 @@ CREATE TABLE `Subrutinas` (
  KEY `id_estado` (`id_estado`),
  CONSTRAINT `Subrutinas_ibfk_1` FOREIGN KEY (`id_rutina`) REFERENCES `Rutinas` (`id`),
  CONSTRAINT `Subrutinas_ibfk_2` FOREIGN KEY (`id_ejercicio`) REFERENCES `Ejercicios` (`id`),
- CONSTRAINT `Subrutinas_ibfk_3` FOREIGN KEY (`id_estado`) REFERENCES `Estados` (`id`)
+ CONSTRAINT `Subrutinas_ibfk_3` FOREIGN KEY (`id_estado`) REFERENCES `EstadosSubrutina` (`id`)
 );
 INSERT INTO Sexo (Sexo) VALUES ('Masculino');
 INSERT INTO Sexo (Sexo) VALUES ('Femenino');
@@ -201,63 +215,30 @@ INSERT INTO TiposDeObjetivos (Tipo) VALUES ('Mantener la figura');
 INSERT INTO Logros (Logro, Descripcion, URL) VALUES ('Medalla Dedicación','¡Cumplíste todos los objetivos!','img/medalla_dedicacion.png');
 INSERT INTO Logros (Logro, Descripcion, URL) VALUES ('Medalla Constancia','¡Asistencia Perfecta!','img/medalla_constancia.png');
 INSERT INTO Logros (Logro, Descripcion, URL) VALUES ('Medalla Creído','¡Te ves muy bien!','img/medalla_creido.png');
-INSERT INTO Estados (Estado) VALUES ('Pausado');
-INSERT INTO Estados (Estado) VALUES ('En proceso');
-INSERT INTO Estados (Estado) VALUES ('Finalizado');
-INSERT INTO Estados (Estado) VALUES ('Cancelado');
+INSERT INTO EstadosObjetivos (Estado) VALUES ('No cumplido');
+INSERT INTO EstadosObjetivos (Estado) VALUES ('Cumplido');
+INSERT INTO EstadosSubrutina (Estado) VALUES ('Pausado');
+INSERT INTO EstadosSubrutina (Estado) VALUES ('En proceso');
+INSERT INTO EstadosSubrutina (Estado) VALUES ('Finalizado');
+INSERT INTO EstadosSubrutina (Estado) VALUES ('Cancelado');
 INSERT INTO Ejercicios (Nombre, id_exigencia, NivelAerobico, TiempoUnitario, id_grupo_muscular) VALUES ('Press inclinado', 3, 8, 18, 1);
 INSERT INTO Ejercicios (Nombre, id_exigencia, NivelAerobico, TiempoUnitario, id_grupo_muscular) VALUES ('Press declinado', 3, 8, 12, 1);
 INSERT INTO Ejercicios (Nombre, id_exigencia, NivelAerobico, TiempoUnitario, id_grupo_muscular) VALUES ('Flexiones', 2, 5, 10, 1);
 INSERT INTO Ejercicios (Nombre, id_exigencia, NivelAerobico, TiempoUnitario, id_grupo_muscular) VALUES ('Press de hombros', 2, 5, 6, 2);
 INSERT INTO Ejercicios (Nombre, id_exigencia, NivelAerobico, TiempoUnitario, id_grupo_muscular) VALUES ('Press Arnold', 2, 5, 6, 2);
-INSERT INTO Usuarios 
-(Nombre, Apellido, Sexo, FechaDeNacimiento, DNI, Direccion, Celular, UserName, Password, PrimeraPregunta, SegundaPregunta, TerceraPregunta, Peso, PorcentajeGrasaCorporal, IndiceMasaCorporal, FechaDeRegistro) VALUES ('Jeremias', 'Mazzetti', '1', '1992-06-01', '55555555', 'Calle 1500', '15-6666-6666', 'admin', '123456', '1', '2', '5', '75.5', '5.0', '75.0', '2022-09-03');
+INSERT INTO Usuarios (Nombre, Apellido, Sexo, FechaDeNacimiento, DNI, Direccion, Email, Celular, FechaDeRegistro) VALUES ('Jeremias', 'Mazzetti', '1', '1992-06-01', '3636363636', 'Calle 1500', 'jere.mazzetti@gmail.com', '15-6666-6666','2022-09-04');
+INSERT INTO LogIn (id_usuario, UserName, Password, PrimeraPregunta, SegundaPregunta, TerceraPregunta, Provisoria) VALUES ('1', 'admin', '123456', '1', '2', '5', '0');
+INSERT INTO EstadosUsuarios (id_usuario, id_sexo, Altura, Peso, IndicenMasaCorporal, PorcentajeGrasaCorporal) VALUES ('1','1','1.77','75.0','75.5','5');
 INSERT INTO RespuestasDeUsuarios (id_usuario, id_pregunta, Respuesta) VALUES ('1', '1', 'Azul Noche');
 INSERT INTO RespuestasDeUsuarios (id_usuario, id_pregunta, Respuesta) VALUES ('1', '2', 'Martin');
 INSERT INTO RespuestasDeUsuarios (id_usuario, id_pregunta, Respuesta) VALUES ('1', '5', 'Fulanito');
 INSERT INTO Objetivos (id_tipo, PesoIdeal, MasaCorporalIdeal, MargenAceptable, id_estado) VALUES ('2', '72.5', '80.5', '3.5', '1');
-INSERT INTO Entrenamientos (fechaDeInicio, FechaDeFin, id_usuario, id_objetivo, id_estado) VALUES ('2022-09-13', '2022-10-04', '1','1', '1');
-INSERT INTO Rutinas (FechaProgramada, id_dia, id_entrenamiento, id_estado) VALUES ('2022-09-13', '2', '1', '1');
-INSERT INTO Rutinas (FechaProgramada, id_dia, id_entrenamiento, id_estado) VALUES ('2022-09-15', '4', '1', '1');
+INSERT INTO Entrenamientos (fechaDeInicio, FechaDeFin, id_usuario, id_objetivo) VALUES ('2022-09-13', '2022-10-04', '1','1');
+INSERT INTO Rutinas (FechaProgramada, id_dia, id_entrenamiento) VALUES ('2022-09-13', '2', '1');
+INSERT INTO Rutinas (FechaProgramada, id_dia, id_entrenamiento) VALUES ('2022-09-15', '4', '1');
 INSERT INTO Subrutinas (id_rutina, id_ejercicio, Serie, Repeticiones, Esfuerzo, id_estado) VALUES ('1', '1', '20', '3', '10.5', '1');
 INSERT INTO Subrutinas (id_rutina, id_ejercicio, Serie, Repeticiones, Esfuerzo, id_estado) VALUES ('1', '2', '15', '2', '9.5', '1');
 INSERT INTO Subrutinas (id_rutina, id_ejercicio, Serie, Repeticiones, Esfuerzo, id_estado) VALUES ('1', '3', '10', '4', '12.5', '1');
 INSERT INTO Subrutinas (id_rutina, id_ejercicio, Serie, Repeticiones, Esfuerzo, id_estado) VALUES ('2', '4', '15', '2', '8.5', '1');
 INSERT INTO Subrutinas (id_rutina, id_ejercicio, Serie, Repeticiones, Esfuerzo, id_estado) VALUES ('2', '2', '10', '2', '10.0', '1');
-INSERT INTO Subrutinas (id_rutina, id_ejercicio, Serie, Repeticiones, Esfuerzo, id_estado) VALUES ('2', '5', '20', '3', '12.5', '1');
-
-/*
-CONSULTA DE INTERÉS PARÁMETRO (USERNAME)
-
-SELECT 
-    	U.Nombre,
-    	U.UserName As Usuario,
-    	TOB.Tipo AS Objetivo,
-    	ES.Estado AS Estado,
-    	D.Dia AS Día,
-    	R.FechaProgramada AS Fecha,
-    	EJ.Nombre AS Ejercicio
-FROM 
-    	Usuarios AS U, 
-    	Entrenamientos AS E, 
-    	Rutinas AS R,
-    	Objetivos AS O,
-    	TiposDeObjetivos AS TOB,
-    	Subrutinas AS SR,
-    	Ejercicios AS EJ,
-    	DiasHabilitados AS D,
-   	Estados AS ES
-WHERE
-	U.UserName='[PARAM]' AND
-	U.id=E.id_usuario AND
-    	E.id=R.id_entrenamiento AND
-  	E.id_estado=ES.id AND
-    	(ES.Estado='Pausado' OR ES.Estado='En proceso') AND
-    	E.id_objetivo=O.id AND
-    	O.id_tipo=TOB.id AND 
-    	R.id=SR.id_rutina AND
-    	R.FechaProgramada='2022-09-15' AND
-    	R.id_dia=D.id AND
-    	SR.id_ejercicio=EJ.id
-    	
-*/	
+INSERT INTO Subrutinas (id_rutina, id_ejercicio, Serie, Repeticiones, Esfuerzo, id_estado) VALUES ('2', '5', '20', '3', '12.5', '1');	
